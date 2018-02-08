43167578cc69701c293340d596494f3b68401bab
SMeredith <stani.instructor@gmail.com>
2018-02-07 18:00:24 -0800

    with second activity

--- app/src/main/java/com/example/smeredith/sumcalculator/MainActivity.java
+++ app/src/main/java/com/example/smeredith/sumcalculator/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.smeredith.sumcalculator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -8,7 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity  {
 
     //define variables for the widgets
     private EditText editTextNum1;
@@ -35,24 +36,37 @@ public class MainActivity extends Activity implements View.OnClickListener {
         textViewSum = (TextView) findViewById(R.id.textViewSum);
 
         //set the listener
-        button.setOnClickListener((View.OnClickListener) this);
+      //  button.setOnClickListener((View.OnClickListener) this);
 
         //get SharedPreferences object
         savedValues = getSharedPreferences("SavedValues", MODE_PRIVATE);
     }
 
-    public void calculateAndDisplay(){
+    /*public void calculateAndDisplay(){
         num1 = Integer.parseInt(editTextNum1.getText().toString());
         num2 = Integer.parseInt(editTextNum2.getText().toString());
         int sum = num1 + num2;
 
         textViewSum.setText(String.valueOf(sum));
 
-    }
+
+    } */
+
+    public void sendMessage(View view){
+        //set an intent that you are going to invoke the second activity from the first activity
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        //get values for the numbers to be added
+        num1 = Integer.parseInt(editTextNum1.getText().toString());
+        num2 = Integer.parseInt(editTextNum2.getText().toString());
+
+        intent.putExtra("num1", num1);
+        intent.putExtra("num2", num2);
+
+        startActivity(intent);
 
 
-    @Override
-    public void onClick(View view) {
-        calculateAndDisplay();
+
+
     }
+
 }
